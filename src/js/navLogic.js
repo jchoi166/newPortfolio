3bc11a99af4b5b3f1973019de621527c5cedb283
Spike <jchoi166@gmail.com>
2020-12-03 07:50:04 -0800

    added project bios

--- src/js/navLogic.js
+++ src/js/navLogic.js
@@ -11,46 +11,42 @@ const aboutLink = document.querySelector('#link-about')
 const portLink = document.querySelector('#link-portfolio')
 const contactLink = document.querySelector('#link-contact')
 
-var homeView = home.getBoundingClientRect();
-var aboutView = about.getBoundingClientRect();
-var portView = portfolio.getBoundingClientRect();
-var contactView = contact.getBoundingClientRect();
-console.log(aboutView)
-console.log(portView)
-console.log(contactView)
 
 window.onscroll = function () { 
-    // "use strict";
-    console.log(window.scrollY)
+    // console.log(window.scrollY)
+
+    // Switching Nav Bars
     if (window.scrollY >= 200 ) {
-        // console.log("its past 200")
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
     } else {
-        // console.log("its back")
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
     }
     
+    // Underline for Home Tab
     if (window.scrollY < 700) {
         homeLink.classList.add("link-active")
     } else {
         homeLink.classList.remove("link-active")
     }
 
-    if (window.scrollY > 700 && window.scrollY < 1300) {
+    // Underline for About Tab
+    if (window.scrollY >= 700 && window.scrollY < 1300) {
         aboutLink.classList.add("link-active")
     } else {
         aboutLink.classList.remove("link-active")
     }
 
-    if (window.scrollY > 1300 && window.scrollY < 2100) {
+    // Underline for Portfolio Tab
+    if (window.scrollY >= 1300 && window.scrollY < 2100) {
         portLink.classList.add("link-active")
     } else {
         portLink.classList.remove("link-active")
     }
 
-    if (window.scrollY > 2100) {
+    // Underline for Contact Tab
+    if (window.scrollY >= 2100) {
         contactLink.classList.add("link-active")
     } else {
         contactLink.classList.remove("link-active")
