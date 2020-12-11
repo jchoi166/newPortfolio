2244e4ee5b6b9e727f72c55441d2178d4ba9fe48
Spike <jchoi166@gmail.com>
2020-12-11 08:26:22 -0800

    almost done, need to work on mobile nav, popout modal?

--- src/js/navLogic.js
+++ src/js/navLogic.js
@@ -12,45 +12,47 @@ const portLink = document.querySelector('#link-portfolio')
 const contactLink = document.querySelector('#link-contact')
 
 
-window.onscroll = function () { 
-    // console.log(window.scrollY)
+window.onscroll = () => { 
 
-    // Switching Nav Bars
-    if (window.scrollY >= 200 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-    }
+        // Switching Nav Bars
+        if (window.scrollY >= 200 ) {
+            myNav.classList.add("nav-colored");
+            myNav.classList.remove("nav-transparent");
+        } else {
+            myNav.classList.add("nav-transparent");
+            myNav.classList.remove("nav-colored");
+        }
+        
+        // Underline for Home Tab
+        if (window.scrollY < 700) {
+            homeLink.classList.add("link-active")
+        } else {
+            homeLink.classList.remove("link-active")
+        }
     
-    // Underline for Home Tab
-    if (window.scrollY < 700) {
-        homeLink.classList.add("link-active")
-    } else {
-        homeLink.classList.remove("link-active")
-    }
+        // Underline for About Tab
+        if (window.scrollY >= 700 && window.scrollY < 1300) {
+            aboutLink.classList.add("link-active")
+        } else {
+            aboutLink.classList.remove("link-active")
+        }
+    
+        // Underline for Portfolio Tab
+        if (window.scrollY >= 1300 && window.scrollY < 2100) {
+            portLink.classList.add("link-active")
+        } else {
+            portLink.classList.remove("link-active")
+        }
+    
+        // Underline for Contact Tab
+        if (window.scrollY >= 2100) {
+            contactLink.classList.add("link-active")
+        } else {
+            contactLink.classList.remove("link-active")
+        }
+    // }
 
-    // Underline for About Tab
-    if (window.scrollY >= 700 && window.scrollY < 1300) {
-        aboutLink.classList.add("link-active")
-    } else {
-        aboutLink.classList.remove("link-active")
-    }
-
-    // Underline for Portfolio Tab
-    if (window.scrollY >= 1300 && window.scrollY < 2100) {
-        portLink.classList.add("link-active")
-    } else {
-        portLink.classList.remove("link-active")
-    }
-
-    // Underline for Contact Tab
-    if (window.scrollY >= 2100) {
-        contactLink.classList.add("link-active")
-    } else {
-        contactLink.classList.remove("link-active")
-    }
+    
 
 };
 
